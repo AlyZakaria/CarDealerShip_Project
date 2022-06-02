697a69203344419da0811eabca1b650ff89bc458
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2022-06-02 15:19:14 +0200

    handled if there is no images/ folder

--- src/main/java/com/example/project/HelloApplication.java
+++ src/main/java/com/example/project/HelloApplication.java
@@ -1,7 +1,6 @@
 package com.example.project;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
